3af5284d3c293aeb0bb63a086de723ab283bec68
Stratos-Chen <twinschen55@gmail.com>
2020-05-27 10:38:26 -0700

    created routes to display JSON for individual products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,19 @@ class Api::ProductsController < ApplicationController
     @products_list = Product.all
     render json: @products_list
   end
+
+  def product_one
+    @find_product = Product.find_by(id: 1)
+    render json: @find_product
+  end
+
+  def product_two
+    @find_product = Product.find_by(id: 2)
+    render json: @find_product
+  end
+
+  def product_three
+    @find_product = Product.find_by(id: 3)
+    render json: @find_product
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
   # end
   namespace :api do
     get "/products" => "products#product_list"
+    get "/product1" => "products#product_one"
+    get "/product2" => "products#product_two"
+    get "/product3" => "products#product_three"
   end
 end
